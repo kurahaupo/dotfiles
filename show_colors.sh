5c1b7e27a757ec4f17c3b8d0bca72e6829acd79b
Martin Kealey <from-github@kurahaupo.gen.nz>
2022-08-03 13:11:30 +1000

    Update show_colors.sh

--- show_colors.sh
+++ show_colors.sh
@@ -8,26 +8,25 @@
 #   colors (default + 8 escapes).
 #
 
-T='gYw'   # The test text
-# Ascii escape char is 27, Hex \x1B, Octal 033
+T=gYw   # The test text
+# ASCII escape char is 27, Hex \x1B, Octal 033
 
-COLOR_NAMES=('' Black Red Green Yellow Blue Magenta Cyan White)
-LIGHT_DARK=('' Bright)
-echo -e "\n                 40m     41m     42m     43m\
-     44m     45m     46m     47m";
-FGS=('    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
-     '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-     '  36m' '1;36m' '  37m' '1;37m')
-for i in "${!FGS[@]}"
-  do
-  FG=${FGS[$i]// /}
-  c=$((i/2))
-  if [ $i -gt 2 ]; then l=$((i-2)); fi
-  printf -v COLOR "%15s" "${LIGHT_DARK[$((l%2))]} ${COLOR_NAMES[$c]}"
-  echo -en " $COLOR ${FGS[$i]} \033[$FG  $T  "
-  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
-    do echo -en " \033[$FG\033[$BG  $T  \033[0m";
+ColorNames=( Black Red Green Yellow Blue Magenta Cyan White - Default )
+LightOrDark=( '' 'Bright ' )
+
+printf '\n                '
+printf ' %-7.7s' "${ColorNames[9]}" "${ColorNames[@]:0:8}"
+printf '\n            '
+printf ' %7u' 49 {40..47}
+printf '\n'
+
+for fg in 39 {30..37} ; do
+  for ld in 0 1 ; do
+    printf '%-15s ' "${LightOrDark[ld]}${ColorNames[fg-30]}"
+    for bg in 49 {40..47}; do
+      printf '\e[%u;%u;%um  %-5s\e[m ' "$fg" "$bg" "$((ld?1:22))" "$T"
+    done
+    printf '\e[m\n'
   done
-  echo;
 done
 echo
