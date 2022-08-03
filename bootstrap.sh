ac6c3694ffd951dd45aea2757a75e39819bf4c14
Martin Kealey <from-github@kurahaupo.gen.nz>
2022-08-03 12:15:18 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
-[ -d ~/home ] || git clone https://github.com/JohnMorales/dotfiles ~/home
+[[ -d ~/home ]] ||
+ git clone https://github.com/JohnMorales/dotfiles ~/home
 cd ~/home
-[ -f /etc/redhat-release ] && grep CentOS /etc/redhat-release >/dev/null && ./install_dependencies_redhat.sh
-[ -f /etc/os-release ] && grep Debian /etc/os-release >/dev/null && ./install_dependencies_debian.sh
+[[ -f /etc/redhat-release ]] &&
+ grep CentOS /etc/redhat-release >/dev/null &&
+  ./install_dependencies_redhat.sh
+[[ -f /etc/os-release ]] &&
+ grep Debian /etc/os-release >/dev/null &&
+  ./install_dependencies_debian.sh
 git pull
-which chef >/dev/null || ./profile_init.sh
+command -v chef >/dev/null ||
+ ./profile_init.sh
 chef-client -z --config-option cookbook_path=$PWD/cookbooks -r profile::setup -l info
